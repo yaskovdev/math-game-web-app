e8ff0445b3051d0b8199544bb1e8bac28bc9bbd7
Sergei Iaskov <sergei.iaskov@omniva.ee>
2019-01-28 13:59:15 +0200

    Open WebSocket connection.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const connect = () => {
+	const WebSocket = window.WebSocket || window.MozWebSocket;
+	const connection = new WebSocket('ws://localhost:8080');
+
+	connection.onopen = () => {
+		console.log('Connection opened');
+	};
+
+	connection.onerror = error => {
+		console.log('Error occurred', error);
+	};
+
+	connection.onmessage = message => {
+		console.log('Message received', message.data)
+	};
+};
+
+connect();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
