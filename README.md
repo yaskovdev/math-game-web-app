e42da297c175350136954253e8bbfcffa59040d1
yaskovdev <yaskovdev@gmail.com>
2019-01-28 14:25:02 +0200

    Add ESLint config.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # math-game-web-app
+
 Real-time browser-based math game for up to 10 concurrent users
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 const connect = () => {
-	const WebSocket = window.WebSocket || window.MozWebSocket;
-	const connection = new WebSocket('ws://localhost:8080');
+    const WebSocket = window.WebSocket || window.MozWebSocket
+    const connection = new WebSocket('ws://localhost:8080')
 
-	connection.onopen = () => {
-		console.log('Connection opened');
-	};
+    connection.onopen = () => {
+        console.log('Connection opened')
+    }
 
-	connection.onerror = error => {
-		console.log('Error occurred', error);
-	};
+    connection.onerror = error => {
+        console.log('Error occurred', error)
+    }
 
-	connection.onmessage = message => {
-		console.log('Message received', message.data)
-	};
-};
+    connection.onmessage = message => {
+        console.log('Message received', message.data)
+    }
+}
 
-connect();
+connect()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
