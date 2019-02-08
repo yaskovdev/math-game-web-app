b97fbf85077b768b7a7dd5f93dcc9133ab5a66dc
yaskovdev <yaskovdev@gmail.com>
2019-02-08 20:38:19 +0200

    Updates

---
+++ precache-manifest.86523f08efacafc6592b542bc844f0a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8abf8d443df6415c6639780973245e50",
+    "url": "/static/media/logo.8abf8d44.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "208e58d3c473394ac14a",
+    "url": "/static/js/main.208e58d3.chunk.js"
+  },
+  {
+    "revision": "8af516967e7a8144e18b",
+    "url": "/static/js/1.8af51696.chunk.js"
+  },
+  {
+    "revision": "208e58d3c473394ac14a",
+    "url": "/static/css/main.30ff4ac9.chunk.css"
+  },
+  {
+    "revision": "8af516967e7a8144e18b",
+    "url": "/static/css/1.300204cf.chunk.css"
+  },
+  {
+    "revision": "d029d47a39b9b96c439ddddde0e6e9f1",
+    "url": "/index.html"
+  }
+];
