fa12b8f3d6cfd694476ef75859465fd4c2c776c0
Sergei Iaskov <sergei.iaskov@omniva.ee>
2019-02-08 15:01:14 +0200

    Add the required 10 users limitation.

--- src/App.js
+++ src/App.js
@@ -9,116 +9,126 @@ import { API_URL } from './config'
 
 class App extends PureComponent {
 
-    state = {
-        joined: false,
-        user: {},
-        challenge: null,
-        ratingTable: [],
-        waitingForNewRound: true,
-        userGaveAnswer: false
-    }
+	state = {
+		error: false,
+		joined: false,
+		user: {},
+		challenge: null,
+		ratingTable: [],
+		waitingForNewRound: true,
+		userGaveAnswer: false
+	}
 
-    render() {
-        const { joined, user, challenge, ratingTable, result, waitingForNewRound, userGaveAnswer } = this.state
-        return (
-            <Container>
-                <Row>
-                    <Col className="text-center">
-                        {joined && <div>
-                            <p>
-                                <h4>{user.name}</h4>
-                            </p>
-                            <p>
-                                {waitingForNewRound ?
-                                    <RoundSummary value={result}/> : this.format(challenge, userGaveAnswer)}
-                            </p>
-                            <p>
-                                <ButtonGroup size={'lg'}>
-                                    <Button onClick={() => this.answer(true)}
-                                        disabled={userGaveAnswer || waitingForNewRound}>True</Button>
-                                    <Button onClick={() => this.answer(false)}
-                                        disabled={userGaveAnswer || waitingForNewRound}>False</Button>
-                                </ButtonGroup>
-                            </p>
-                        </div>}
-                        {!joined && <Fragment>
-                            <Row className={'App-logo-row'}>
-                                <Col>
-                                    <img src={logo} alt="Math Game Logo"/>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <Button onClick={this.join}>Let&#39;s play a game!</Button>
-                                </Col>
-                            </Row>
-                        </Fragment>}
-                        {joined && <Button color="link" onClick={this.leave}>Leave the game</Button>}
-                        {joined && ratingTable.length > 0 && <Table>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Score</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {ratingTable.map(user => {
-                                    const current = user.id === this.state.user.id
-                                    return <tr key={user.id}
-                                        className={current ? 'App-user-table-record' : ''}>
-                                        <td>{current ? user.name + ' (you)' : user.name}</td>
-                                        <td>{user.score}</td>
-                                    </tr>
-                                })}
-                            </tbody>
-                        </Table>}
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+	render() {
+		const { joined, user, challenge, ratingTable, result, waitingForNewRound, userGaveAnswer, error } = this.state
+		return (
+			<Container>
+				<Row>
+					<Col className="text-center">
+						{joined && <div>
+							<p>
+								<h4>{user.name}</h4>
+							</p>
+							<p>
+								{waitingForNewRound ?
+									<RoundSummary value={result} /> : this.format(challenge, userGaveAnswer)}
+							</p>
+							<p>
+								<ButtonGroup size={'lg'}>
+									<Button onClick={() => this.answer(true)}
+											disabled={userGaveAnswer || waitingForNewRound}>True</Button>
+									<Button onClick={() => this.answer(false)}
+											disabled={userGaveAnswer || waitingForNewRound}>False</Button>
+								</ButtonGroup>
+							</p>
+						</div>}
+						{!joined && <Fragment>
+							<Row className={'App-logo-row'}>
+								<Col>
+									<img src={logo} alt="Math Game Logo" />
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<Button onClick={this.join}>Let&#39;s play a game!</Button>
+								</Col>
+							</Row>
+							{error && <Row className={'App-error-row'}>
+								<Col>
+									An error occurred. Most likely the user limit was reached. Try again later.
+								</Col>
+							</Row>}
+						</Fragment>}
+						{joined && <Button color="link" onClick={this.leave}>Leave the game</Button>}
+						{joined && ratingTable.length > 0 && <Table>
+							<thead>
+							<tr>
+								<th>Name</th>
+								<th>Score</th>
+							</tr>
+							</thead>
+							<tbody>
+							{ratingTable.map(user => {
+								const current = user.id === this.state.user.id
+								return <tr key={user.id}
+										   className={current ? 'App-user-table-record' : ''}>
+									<td>{current ? user.name + ' (you)' : user.name}</td>
+									<td>{user.score}</td>
+								</tr>
+							})}
+							</tbody>
+						</Table>}
+					</Col>
+				</Row>
+			</Container>
+		)
+	}
 
-    join = () => {
-        const WebSocket = window.WebSocket || window.MozWebSocket
-        const connection = new WebSocket(API_URL)
-        this.setState({ connection })
+	join = () => {
+		const WebSocket = window.WebSocket || window.MozWebSocket
+		const connection = new WebSocket(API_URL)
+		this.setState({ connection })
 
-        connection.onopen = () => {
-            this.setState({ joined: true })
-        }
+		connection.onopen = () => {
+			this.setState({ joined: true })
+		}
 
-        connection.onmessage = message => {
-            const json = JSON.parse(message.data)
-            const { type } = json
+		connection.onmessage = message => {
+			const json = JSON.parse(message.data)
+			const { type } = json
 
-            if (type === 'WELCOME') {
-                const { user, ratingTable } = json
-                this.setState({ user, ratingTable })
-            } else if (type === 'START_ROUND') {
-                const { challenge } = json
-                this.setState({ challenge, waitingForNewRound: false })
-            } else if (type === 'END_ROUND') {
-                const { result, ratingTable } = json
-                this.setState({ result, ratingTable, waitingForNewRound: true, userGaveAnswer: false })
-            } else {
-                throw new Error('unexpected message type ' + type)
-            }
-        }
-    }
+			if (type === 'WELCOME') {
+				const { user, ratingTable } = json
+				this.setState({ user, ratingTable })
+			} else if (type === 'START_ROUND') {
+				const { challenge } = json
+				this.setState({ challenge, waitingForNewRound: false })
+			} else if (type === 'END_ROUND') {
+				const { result, ratingTable } = json
+				this.setState({ result, ratingTable, waitingForNewRound: true, userGaveAnswer: false })
+			} else {
+				throw new Error('unexpected message type ' + type)
+			}
+		}
 
-    leave = () => {
-        this.setState({ joined: false })
-        const { connection } = this.state
-        connection.close()
-    }
+		connection.onerror = () => {
+			this.setState({ error: true })
+		}
+	}
 
-    format = (challenge, userGaveAnswer) => userGaveAnswer ? <AnswerAccepted/> : <Challenge value={challenge}/>
+	leave = () => {
+		this.setState({ joined: false, error: false })
+		const { connection } = this.state
+		connection.close()
+	}
 
-    answer = (agree) => {
-        this.setState({ userGaveAnswer: true })
-        const { connection } = this.state
-        connection.send(agree)
-    }
+	format = (challenge, userGaveAnswer) => userGaveAnswer ? <AnswerAccepted /> : <Challenge value={challenge} />
+
+	answer = (agree) => {
+		this.setState({ userGaveAnswer: true })
+		const { connection } = this.state
+		connection.send(agree)
+	}
 }
 
 export default App
