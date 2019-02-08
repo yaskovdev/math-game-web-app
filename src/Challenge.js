f95cf860c4314dd80b9ef7be9fbccc0eb508a291
yaskovdev <yaskovdev@gmail.com>
2019-02-08 19:45:39 +0200

    Refactor.

--- src/Challenge.js
+++ src/Challenge.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Challenge = props => {
     const { value } = props
-    return <h1>{value.question} = {value.answer}?</h1>
+    return <h1>{value.question} = {value.suggestedAnswer}?</h1>
 }
 
 export default Challenge
--- src/RoundSummary.js
+++ src/RoundSummary.js
@@ -7,12 +7,12 @@ export default class RoundSummary extends PureComponent {
         return <h1>{this.formatWaitingMessage(value)}</h1>
     }
 
-    formatWaitingMessage = (result) => {
-        if (result === 'CORRECT_FIRST_ANSWER') {
+    formatWaitingMessage = (roundSummary) => {
+        if (roundSummary === 'CORRECT_FIRST_ANSWER') {
             return <span className={'text-success'}>Correct answer!</span>
-        } else if (result === 'CORRECT_LATE_ANSWER') {
+        } else if (roundSummary === 'CORRECT_LATE_ANSWER') {
             return <span className={'text-warning'}>Correct, but too late...</span>
-        } else if (result === 'WRONG_ANSWER') {
+        } else if (roundSummary === 'WRONG_ANSWER') {
             return <span className={'text-danger'}>Wrong answer.</span>
         } else {
             return <span>Waiting for a new round...</span>
